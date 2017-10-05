8db8d37ef7a2f9c920e5559f2961ef3343b1342e
wangshiyu <wangsy@tju.edu.cn>
2017-10-06 04:09:49 +0800

    self defined knn

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,33 +1,72 @@
-import cv2
 import numpy as np
 import scipy.io as sio
+import operator
+import math
 
-K=1
-# load the data
-mnist_data = sio.loadmat('mnist_data.mat')
+K=13  #for running in different parameters, modify this K
 
-#seperate training data and test data
-train_array = np.array(mnist_data['train'])
-test_array = np.array(mnist_data['test'])
-train_data = train_array[:,1:785].astype(np.float32)
-train_label = train_array[:,0].astype(np.float32)
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(1,length+1):
+        #distance+=pow((instance1[x]-instance2[x]),2)
+        distance += abs(instance1[x]-instance2[x])
+    #return math.sqrt(distance)
+    return distance
+ 
+def getNeighbors(trainingSet, testInstance, k):
+    distance=[]
+    length = len(testInstance)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distance.append((trainingSet[x],dist))
+    distance.sort(key=operator.itemgetter(1))
+    neighbors=[]
+    for x in range(k):
+        neighbors.append(distance[x][0])
+        return neighbors
 
-#create random data
-random_numbers = np.random.choice(10000,100)
-random_data = test_array[random_numbers].astype(np.float32)
-test_data = random_data[:, 1:785]
-test_label = random_data[:,0]
+def getResponse(neighbors):
+    classVotes={}
+    for x in range(len(neighbors)):
+        response = neighbors[x][0]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1),
+            reverse=True)
+    return sortedVotes[0][0]
 
-#knn
-knn=cv2.KNearest()
-knn.train(train_data, train_label)
+def getAccuracy(testSet, predictions):
+    correct=0
+    for x in range(len(testSet)):
+        if testSet[x][0]==predictions[x]:
+            correct+=1
+    return (correct/float(len(testSet))) *100
 
-ret, results, neighbors, dist = knn.find_nearest(test_data, K)
+def main():
+    # load the data
+    mnist_data = sio.loadmat('mnist_data.mat')
 
-#calculate accuracy
-count = 0
-for i in range(0,100):
-    if results[i]==test_label[i]:
-        count=count+1
+    #seperate training data and test data
+    train_array = np.array(mnist_data['train'])
+    test_array = np.array(mnist_data['test'])
+  
+    #create random data
+    random_numbers = np.random.choice(10000,100)
+    test_data = test_array[random_numbers]
+    
+    predictions = []
+    
+    for x in range(len(test_data)):
+        neighbors = getNeighbors(train_array, test_data[x], K)
+        result = getResponse(neighbors)
+        predictions.append(result)
+        print('>predicted ='+repr(result) + ',actual=' +repr(test_data[x][0]))
 
-print count,"%"
+    accuracy = getAccuracy(test_data, predictions)
+    print('Accuracy:'+repr(accuracy)+'%')
+
+
+if __name__ == "__main__":
+    main()
